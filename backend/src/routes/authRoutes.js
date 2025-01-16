9fe10b94dbcde9798cae22a36a4503e7730998f8
Mateusz Obirek <neutralnie7@gmail.com>
2025-01-16 14:10:18 +0100

    added register route

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -4,4 +4,26 @@ import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 8);
+
+  try {
+    const user = await prisma.users.create({
+      data: {
+        username,
+        password: hashedPassword,
+      },
+    });
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+    res.json({ token });
+  } catch (error) {
+    console.log(error.message);
+    res.send(503);
+  }
+});
+
 export default router;
