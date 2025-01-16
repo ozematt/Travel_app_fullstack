78b953b514e2e1bb8a7f549313fadfb3f5d85a16
Mateusz Obirek <neutralnie7@gmail.com>
2025-01-16 14:46:44 +0100

    added prisma generate

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,9 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const router = express.Router();
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,20 @@
 import express from "express";
 import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const BACKEND_PORT = process.env.PORT || 5005;
-const FRONTEND_PORT = "http://localhost:5173";
+const BACKEND_PORT = process.env.BACKEND_PORT || 5005;
 
 //Middleware
-app.use(
-  cors({
-    origin: FRONTEND_PORT,
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors());
 
 app.use(express.json()); // Parse JSON
 
 //ROUTES
-app.get("/api/users", (req, res) => {
-  res.json("Działa!");
-});
+app.use("/auth", authRoutes);
 
 app.listen(BACKEND_PORT, () => {
   console.log(`Server has started on port: ${BACKEND_PORT}`);
