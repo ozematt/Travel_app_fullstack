1865a626526e42cd5a656dbf1e09d19f07337dff
Mateusz Obirek <neutralnie7@gmail.com>
2025-01-16 14:16:30 +0100

    added login route

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -26,4 +26,32 @@ router.post("/register", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await prisma.users.findUnique({
+      where: {
+        username: username,
+      },
+    });
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    const passwordIsValid = bcrypt.compareSync(password, user.password);
+    if (!passwordIsValid) {
+      return res.status(401).send({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+    res.json({ token });
+  } catch (error) {
+    console.log(error.message);
+    res.send(503);
+  }
+});
+
 export default router;
