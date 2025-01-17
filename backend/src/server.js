9f8720fa922abb24fd4bc6146d6da01a215e606c
Mateusz Obirek <neutralnie7@gmail.com>
2025-01-17 13:45:02 +0100

    updated prisma schema

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,27 +6,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const BACKEND_PORT = process.env.BACKEND_PORT || 5005;
+const PORT = process.env.BACKEND_PORT || 5005;
 
 //Middleware
 app.use(cors());
-
 app.use(express.json()); // Parse JSON
 
 //ROUTES
-
-// app.post("/api/auth/register", (req, res) => {
-//   console.log(req.body);
-//   res.status(200).json({ message: "Debugging..." });
-// });
-
-// app.get("/", (req, res) => {
-//   console.log("Hello");
-//   res.status(200).json({ message: "Debugging..." });
-// });
-
 app.use("/api/auth", authRoutes);
 
-app.listen(BACKEND_PORT, () => {
-  console.log(`Server has started on port: ${BACKEND_PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server has started on port: ${PORT}`);
 });
